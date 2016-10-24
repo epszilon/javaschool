5cefc99c6ae5d2fc12cb676631de100bad3cac91
Sándor György <sandor.gyorgy@gmail.com>
2016-10-24 14:26:52 +0200

    Screen output is added.

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -17,7 +17,7 @@ public class Project1
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        System.out.println("Project 1");
     }
     
 }
