4c9e80331a66202169b2c087f9c23bbf02498963
Sándor György <sandor.gyorgy@gmail.com>
2016-10-24 14:38:43 +0200

    Project3 display text is added.

--- Project3/src/project3/Project3.java
+++ Project3/src/project3/Project3.java
@@ -17,7 +17,7 @@ public class Project3
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        System.out.println("Project 3");
     }
     
 }
